4bcdd19a2fdf712a4d40668fc0f39dc062d3bf5f
Jan-Willem van der Meer <jw@macbook-jw.lan>
2010-04-26 21:22:02 +0200

    fixed page caching, redirect to programs after login

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    case resource.class.name
+    when 'User'
+      programs_path
+    when 'Admin'
+      admin_root_path
+    end
+  end
+  
 end
