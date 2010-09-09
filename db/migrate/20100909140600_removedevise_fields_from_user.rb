10b3890918cbc8a7f5f8f7677bbc9989a42db845
Jan-Willem van der Meer <jw@jan-willem-van-der-meers-imac.local>
2010-09-09 17:33:37 +0200

    do not include datbase.yml in project

--- db/migrate/20100909140600_removedevise_fields_from_user.rb
+++ db/migrate/20100909140600_removedevise_fields_from_user.rb
@@ -4,9 +4,7 @@ class RemovedeviseFieldsFromUser < ActiveRecord::Migration
     remove_column :users, :reset_password_token
     remove_column :users, :remember_token
     remove_column :users, :remember_created_at
-    remove_column :users, :remember_created_at
     remove_column :users, :sign_in_count
-    remove_column :users, :current_sign_in_at
     remove_column :users, :current_sign_in_at
     remove_column :users, :last_sign_in_at
     remove_column :users, :current_sign_in_ip
