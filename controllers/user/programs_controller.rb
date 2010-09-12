0df9f9f12b84f9633b5c1a6ec5d4c50d0bb28ecf
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-09-12 23:43:16 +0200

    added airs_at attribute and using it

--- controllers/user/programs_controller.rb
+++ controllers/user/programs_controller.rb
@@ -18,8 +18,9 @@ class User::ProgramsController < UserAreaController
   
   def index
     @programs           = @user.programs
-    @upcomming_episodes = Episode.airdate_inside(Date.today, 1.week.from_now).watched_by_user(@user.programs).by_airdate(:desc)
-    @past_episodes      = Episode.airdate_inside(1.week.ago, Date.today).watched_by_user(@user.programs).by_airdate(:desc)
+    basic_episodes      = Episode.watched_by_user(@user.programs).limited(6)
+    @upcomming_episodes = basic_episodes.by_airdate.airdate_after(Time.now)
+    @past_episodes      = basic_episodes.by_airdate(:desc).airdate_before(Time.now)
   end
   
   def destroy
