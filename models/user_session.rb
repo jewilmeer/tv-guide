caab9160ca3246656e7c8c37cba78ccdfa7d4b31
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-09-22 23:29:20 +0200

    test rss feed added

--- models/user_session.rb
+++ models/user_session.rb
@@ -1,5 +1,10 @@
 class UserSession < Authlogic::Session::Base
+  # single_access_allowed_request_types = ['application/x-nzb']
   def to_key
      self.keys.to_a
+  end
+  
+  def single_access_allowed_request_types
+    ['application/x-nzb', "application/rss+xml", "application/atom+xml"]
   end
 end
