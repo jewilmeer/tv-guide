c476866cfd68ac09d069fe32d52a5816b8835fcb
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-10-23 17:02:22 +0200

    enable hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,3 @@
-# HoptoadNotifier.configure do |config|
-#   config.api_key = 'b07b949120e59d4b264b40cb970570c9'
-# end
+HoptoadNotifier.configure do |config|
+  config.api_key = 'b07b949120e59d4b264b40cb970570c9'
+end
