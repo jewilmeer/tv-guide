f8dd9b9ee757b582572ebda013ef87ddc3d9ed1a
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-11-19 00:37:49 +0100

    added an program overview with sorting and search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
   def ui_icon type
     content_tag :button, type.humanize, :class => type
   end
+  
+  def sortable(column, title = nil)
+    title ||= column.to_s.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    link_to title, params.merge({:sort => column, :direction => direction}), {:class => css_class}
+  end
 end
