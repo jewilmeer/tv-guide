c2d1a3d82ff13417d29c3ff0e0e6d178ca00c054
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-11-29 22:51:01 +0100

    more new gem stuff

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,22 @@
 
 ActiveRecord::Schema.define(:version => 20101129182725) do
 
+  create_table "admins", :force => true do |t|
+    t.string   "email",                                            :null => false
+    t.string   "encrypted_password", :limit => 128,                :null => false
+    t.string   "password_salt",                                    :null => false
+    t.integer  "sign_in_count",                     :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",                   :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
     t.string   "provider"
