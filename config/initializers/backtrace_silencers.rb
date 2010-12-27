559a3a429641196cc8e6936542885c810dcec8fe
Jan-Willem van der Meer <jewilmeer@gmail.com>
2010-12-27 16:27:08 +0100

    an other hack for new relic ignore

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,7 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+
+
+# hack for new relic
+ActionController::Base.send :include, NewRelic::Agent::Instrumentation::ControllerInstrumentation
