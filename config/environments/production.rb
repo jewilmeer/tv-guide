7deb0bbeb43ef236a8ffb87464b85e1d1d457002
Jan-Willem van der Meer <jewilmeer@gmail.com>
2011-01-05 00:32:35 +0100

    gmail config for development, maybe it helps

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,6 @@ TvEpisodes::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
   config.action_mailer.default_url_options = { :host => "jewilmeer.com" }
-
   config.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
