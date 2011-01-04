161fe113c209434c30056740390501ac58f31b30
Jan-Willem van der Meer <jewilmeer@gmail.com>
2011-01-04 22:57:01 +0100

    add the initial mailer

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'http://rubygems.org'
 gem 'rails', '~>3.0'
+gem 'mail', '~>2.2'
 gem 'bundler'
 
 # Bundle the extra gems:
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,17 @@
 class UserMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "Digital TV Guide <mail@jewilmeer.com>"
+  
+  def pretty_name user
+    "#{user.login} <#{user.email}>"
+  end
+  
+  def registration_confirmation user
+    @user = user
+    mail( :to => pretty_name(user), :subject => 'Welcome to Digital TV Guide!' )
+  end
+  
+  def trusted_notification user 
+    @user = user
+    mail( :to => pretty_name(user), :subject => 'You can now use \'special\' features' )
+  end
 end
