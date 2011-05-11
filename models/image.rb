e05a7798fe197ff85797d4c0ee26bb0edf8f76f0
jewilmeer <jewilmeer@gmail.com>
2011-05-12 00:39:32 +0200

    newer styling of the website... how cool\!

--- models/image.rb
+++ models/image.rb
@@ -50,6 +50,7 @@ class Image < ActiveRecord::Base
   def save_image
     require 'open-uri'
     open(url) {|tmp_file| self.image= tmp_file}
+    save
   end
 
   def s3_url(image_format = 'banner')
