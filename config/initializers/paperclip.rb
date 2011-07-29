0e7c7961ed2bbf6b7878131846bccd7134a2b046
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-07-29 18:06:35 +0200

    add imagemagick ref for steef

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,8 @@ Paperclip.interpolates :filename do |attachment, style|
   attachment.instance.filename
 end
 Paperclip.options[:log] = true
+
+if ENV['steef']
+  Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.7.1-Q16'
+  Paperclip.options[:swallow_stderr] = false
+end
