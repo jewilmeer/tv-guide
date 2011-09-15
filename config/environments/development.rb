e9a8e1f6c90a3671445ce0b7893621e5bd531aec
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-09-15 22:56:56 +0200

    upgraded to rails 3.1 with asset pipeline

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,6 @@ TvEpisodes::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   config.cache_store = :dalli_store
@@ -31,4 +30,10 @@ TvEpisodes::Application.configure do
   
   # config.cache_store = :mem_cache_store
   config.active_support.deprecation = :log
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
 end
