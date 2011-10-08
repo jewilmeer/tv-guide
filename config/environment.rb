5a6225d147424df5209b9685d735675b0e69fa3c
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-08 15:02:49 +0200

    upgraded to rails 3.1.1
    Signed-off-by: Jan-Willem van der Meer <mail@jewilmeer.nl>

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TvEpisodes::Application.initialize!
-
-ActiveSupport::Dependencies.log_activity = true
-
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  if html_tag.match(/^\<input/)
-    if instance.error_message.kind_of?(Array)
-      %(#{html_tag}<span class="validation-error">#{instance.error_message.join(', ')}</span>).html_safe
-    else
-      %(#{html_tag}<span class="validation-error">#{instance} #{instance.error_message}</span>).html_safe
-    end
-  else
-    html_tag
-  end
-end
