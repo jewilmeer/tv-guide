86c0013f07bebd061e589e39e464bfb4782c2769
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-08 15:28:40 +0200

    Revert "upgraded to rails 3.1.1"
    This reverts commit 8235e3f593eb3181daa8ae7eebc09edc3a18a590.

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'f90cdbe12a49523a65f446318a9725c0821d33f3251d33166ef80e2754bbb898982bbe035bb5b47df6a0e2d6421c4288ec7aab20573a5af14b521f9594e7cddb'
+# Rails.application.config.secret_token = 'f90cdbe12a49523a65f446318a9725c0821d33f3251d33166ef80e2754bbb898982bbe035bb5b47df6a0e2d6421c4288ec7aab20573a5af14b521f9594e7cddb'
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,6 +2,5 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register_alias "text/html", :iphone
 Mime::Type.register_alias "application/x-nzb", :nzb
 Mime::Type.register_alias "image/jpg", :jpg
