4e3b0cf3f3de11882350c8a38f2eeeda56716a1a
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-08 15:28:40 +0200

    Revert "upgraded to rails 3.1.1"
    This reverts commit 8235e3f593eb3181daa8ae7eebc09edc3a18a590.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TvEpisodes::Application.config.secret_token = 'a8b1fe20e8c06e2ca9412f1bbae259a4dc6257b7d56ece50ec115756645ec23b2bfc2ab144be41faa83ece81dc40de12176bc418d41ce62e7c23ca0ba1123c38'
