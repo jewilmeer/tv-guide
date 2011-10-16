7a4c9e04dde33d64173a5346abfb2b5c7cf817e2
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-16 23:25:50 +0200

    remove active admin for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TvEpisodes::Application.routes.draw do
-  ActiveAdmin.routes(self)
+  # ActiveAdmin.routes(self)
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  # devise_for :admin_users, ActiveAdmin::Devise.config
 
   match '/login',           :to => 'user_sessions#new'
   match '/logout',          :to => 'user_sessions#destroy'
