084ebee895b036ca3a345c0c5705766315d92e70
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-16 22:35:31 +0200

    rails 3.1.1 upgrade
    Signed-off-by: Jan-Willem van der Meer <mail@jewilmeer.nl>

--- app/assets/javascripts/lib/bootstrap/bootstrap.js
+++ app/assets/javascripts/lib/bootstrap/bootstrap.js
@@ -1,8 +1,8 @@
-//= require bootstrap-alerts
-//= require bootstrap-dropdown
-//= require bootstrap-modal
-//= require bootstrap-twipsy
-//= require bootstrap-popover
-//= require bootstrap-scrollspy
-//= require bootstrap-tabs
+//= require ./bootstrap-alerts
+//= require ./bootstrap-dropdown
+//= require ./bootstrap-modal
+//= require ./bootstrap-twipsy
+//= require ./bootstrap-popover
+//= require ./bootstrap-scrollspy
+//= require ./bootstrap-tabs
 
