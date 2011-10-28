52d778c42d28aaac9237311e001c83f910d39f58
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-10-28 18:28:06 +0200

    make images cachable

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,3 +2,7 @@ Paperclip.interpolates :filename do |attachment, style|
   attachment.instance.filename
 end
 Paperclip.options[:log] = true
+
+Paperclip.interpolates :timestamp do |attachment, style|
+  attachment.instance_read(:updated_at).to_i  
+end
