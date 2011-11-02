60c841190d03329612bc1f0e585636ed86d40c05
Jan-Willem van der Meer <mail@jewilmeer.nl>
2011-11-02 11:23:14 +0100

    added time control to interactions table

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,9 +1,6 @@
 module DateHelper
   def pretty_date date, options={}
-    date      = date.to_date
-    date_diff = date - Date.today
-
-    case date_diff
+    case date_diff(date)
     when 0
       'Today'
     when -1
@@ -15,11 +12,21 @@ module DateHelper
     when -7..-2
       "Last #{ l(date, :format => :weekday)}"
     else
-      if options[:with_year].present? || date_diff.to_i.abs > 60#days
+      if date_needs_year? date
         l date, :format => :with_day
       else
         l date, :format => :short_with_weekday
       end
     end
+  end
+
+  private 
+
+  def date_diff date
+    date.to_date - Date.today
+  end
+
+  def date_needs_year? date
+    Date.today.year != date.to_date.year 
   end
 end
