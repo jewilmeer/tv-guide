52892bc430c2ab6cf39cbbb5d0f5da9dfe9f2ade
Jan-Willem van der Meer <mail@jewilmeer.nl>
2012-01-13 19:49:06 +0100

    downgrade back to rails 3.1
    Signed-off-by: Jan-Willem van der Meer <mail@jewilmeer.nl>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,10 +41,10 @@ TvEpisodes::Application.configure do
   # Expands the lines which load the assets
   # config.assets.debug = true
   
-  # Raise exception on mass assignment protection for ActiveRecord models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # # Raise exception on mass assignment protection for ActiveRecord models
+  # config.active_record.mass_assignment_sanitizer = :strict
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # # Log the query plan for queries taking more than this (works
+  # # with SQLite, MySQL, and PostgreSQL)
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
