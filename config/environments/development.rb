654fc4d0f7f722daf18bbedba21bd6fb4267fdff
Jan-Willem van der Meer <mail@jewilmeer.nl>
2012-01-20 18:32:30 +0100

    updated rails to rails 32

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,9 +42,9 @@ TvEpisodes::Application.configure do
   # config.assets.debug = true
   
   # # Raise exception on mass assignment protection for ActiveRecord models
-  # config.active_record.mass_assignment_sanitizer = :strict
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # # Log the query plan for queries taking more than this (works
   # # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
