3b3958c4b2f1ca3ec98cdc8e94ad931998a56996
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-01-18 23:36:57 +0100

    Remove more legacy
    Signed-off-by: Jan-Willem van der Meer <mail@jewilmeer.nl>

--- app/controllers/user/authentications_controller.rb
+++
@@ -1,9 +0,0 @@
-class User::AuthenticationsController < UserAreaController
-  def destroy
-    if current_user.authentications.delete( Authentication.find(params[:id]) )
-      redirect_to :back, :notice => 'External service deleted'
-    else
-      redirect_to :back, :error => 'Deletion failed'
-    end    
-  end
-end
