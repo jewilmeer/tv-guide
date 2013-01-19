098bc99aebfe593eea85c3da41caad19eba7fb6b
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-01-19 01:45:02 +0100

    infinite scroll love

--- app/controllers/admin/interactions_controller.rb
+++ app/controllers/admin/interactions_controller.rb
@@ -1,14 +1,14 @@
 class Admin::InteractionsController < AdminAreaController
   helper_method :sort_column, :sort_direction
   def index
-    @interactions = Interaction.order(sort_column + ' ' + sort_direction).limit(50)
+    @interactions = Interaction.page params[:page]
   end
 
   protected
   def sort_column
     Interaction.column_names.include?(params[:sort]) ? params[:sort] : "created_at"
   end
-  
+
   def sort_direction
     %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
