49321527ce209e329e3c8617ff24980460c3e057
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-03-22 08:29:54 +0100

    Fix spec test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
-require 'authlogic/test_case'
 
 describe PagesController do
   describe "#index" do
     before { get :index }
     it { should respond_with :redirect }
-    
+
     context "Not logged in" do
       it { should redirect_to guide_programs_path }
     end
