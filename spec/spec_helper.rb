3405b0cc3f37524696912264e76ee8b6415f144b
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-03-22 09:10:16 +0100

    Fix specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,18 @@
-require 'rubygems'
-
 require 'simplecov'
-SimpleCov.command_name "RSpec"
-SimpleCov.coverage_dir 'coverage/'
-SimpleCov.start 'rails'
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-
-require 'database_cleaner'
-DatabaseCleaner.strategy = :truncation
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  config.mock_with :rspec
+  config.use_transactional_fixtures = true
+
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
 
   # short factory_girl syntax
   config.include FactoryGirl::Syntax::Methods
