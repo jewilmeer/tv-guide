c0d5104e6abd0b86bde5bf6da8587f47cdb0efb8
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-03-24 21:14:11 +0100

    Ensure authentication token

--- spec/models/download_spec.rb
+++ spec/models/download_spec.rb
@@ -1,3 +1,14 @@
+require 'spec_helper'
+
+describe Download do
+  let!(:episode) { create :episode }
+  let(:download) { build :download, episode: episode }
+
+  it "updates the episode upon creation" do
+    expect { download.save }.to change episode, :updated_at
+  end
+end
+
 # == Schema Information
 #
 # Table name: downloads
@@ -14,13 +25,3 @@
 #  updated_at            :datetime
 #
 
-require 'spec_helper'
-
-describe Download do
-  let!(:episode) { create :episode }
-  let(:download) { build :download, episode: episode }
-
-  it "updates the episode upon creation" do
-    expect { download.save }.to change episode, :updated_at
-  end
-end
