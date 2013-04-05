a7b08200b71fd2df8f2eb61e2b51ca6340d18977
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-04-05 14:37:12 +0200

    Change add program modal to javascript responder

--- app/controllers/user/program_preferences_controller.rb
+++ app/controllers/user/program_preferences_controller.rb
@@ -1,17 +1,23 @@
 class User::ProgramPreferencesController < UserAreaController
-
   def index
     @program_preferences = current_user.program_preferences.all#.includes(:program)#.included(:program)
     @search_term_types   = SearchTermType.scoped
   end
 
+  def new
+    @program_preference = current_user.program_preferences.build
+    @program_preference.search_term_type_id= SearchTermType.find_by_code('hd')
+  end
+
   def create
+    # maybe programs#show?
     if params[:program_preference] && params[:program_preference][:program_id].present?
       current_user.program_preferences.create( params[:program_preference] )
       respond_to do |format|
         format.html { redirect_to user_programs_path(current_user), :notice => 'Program Added' }
         format.js { flash[:notice] = 'Program added' }
       end
+    # from search page
     elsif params[:tvdb_id].present?
       @program = Program.find_or_create_by_tvdb_id( params[:tvdb_id] )
       if @program.persisted?
@@ -24,11 +30,15 @@ class User::ProgramPreferencesController < UserAreaController
         format.html { redirect_to user_programs_path(current_user) }
         format.js { render :text => "document.location.href = '#{user_programs_path(current_user)}'" }
       end
+    # from add program modal
     elsif params[:program_preference][:q].present? && params[:program_preference][:search_term_type_id].present?
       @program = Program.search_program(params[:program_preference][:q]).first
       if @program && @program.name.downcase == params[:program_preference][:q].downcase
-        current_user.program_preferences.create( :program_id => @program.id, :search_term_type_id => params[:search_term_type_id] )
-        flash[:notice] = 'Program added'
+        if current_user.program_preferences.create( :program_id => @program.id, :search_term_type_id => params[:program_preference][:search_term_type_id] )
+          flash[:notice] = 'Program added'
+        else
+          flash[:error] = 'Could not add program'
+        end
         respond_to do |format|
           format.html { redirect_to user_programs_path(current_user) }
           format.js { render :text => "document.location.href = '#{user_programs_path(current_user)}'" }
