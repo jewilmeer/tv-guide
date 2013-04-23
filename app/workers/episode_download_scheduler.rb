809036272c6bb8421cd792449f77f38b5e4b1a58
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-04-23 16:56:46 +0200

    Set the time for the download window to 5 minutes

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -2,7 +2,7 @@ class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
   def perform
-    Episode.without_download.airs_at_inside(1.day.ago, Time.now).each do |episode|
+    Episode.without_download.airs_at_inside(5.minutes.ago, Time.now).each do |episode|
       EpisodeDownloader.perform_async episode.id
     end
   end
