0e85f9a8c97792668b67070f60db27e9ff1ede81
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-04-26 16:56:16 +0200

    Apply downloadable scope to sidekiq worker

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -1,8 +1,8 @@
 class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
-  def perform(time_ago=5.minutes.ago)
-    Episode.without_download.airs_at_inside(time_ago, Time.now).each do |episode|
+  def perform(time_ago=2.days.ago)
+    Episode.downloadable.airs_at_inside(time_ago, Time.now).each do |episode|
       EpisodeDownloader.perform_async episode.id
     end
   end
