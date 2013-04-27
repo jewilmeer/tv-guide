64d2f2638760c7f637cf79100636d2a16d3afbff
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-04-27 16:19:50 +0200

    Add better logic for downloading episodes

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -1,7 +1,7 @@
 class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
-  def perform(time_ago=2.days.ago)
+  def perform(time_ago=1.hour.ago)
     Episode.downloadable.airs_at_inside(time_ago, Time.now).each do |episode|
       EpisodeDownloader.perform_async episode.id
     end
