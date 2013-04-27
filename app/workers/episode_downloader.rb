4b6a4ddcbafa3ff5bed501ccec538064fa2ba7a9
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-04-27 16:19:50 +0200

    Add better logic for downloading episodes

--- app/workers/episode_downloader.rb
+++ app/workers/episode_downloader.rb
@@ -3,6 +3,13 @@ class EpisodeDownloader
   sidekiq_options backtrace: true
 
   def perform episode_id
-    Episode.find(episode_id).download_all
+    episode = Episode.find(episode_id)
+    # we only want an initial download
+    return if episode.downloads.any?
+
+    # reschedule if not downloaded and within 7 days or airing
+    if !episode.download('hd') && episode.airs_at...episode.max_download_time === Time.now
+      episode.delay_for(1.hour).download('hd')
+    end
   end
 end
