84e9c70616db4c64e0d6ad5fe2ea3b5cf48c2003
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-05-01 22:25:13 +0200

    Reschedule downloads more then once, for each our, until rotten

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -2,7 +2,8 @@ class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
   def perform(time_ago=1.hour.ago)
-    Episode.downloadable.airs_at_inside(time_ago, Time.now).each do |episode|
+    Rails.logger.fatal "Found #{Episode.downloadable.airs_at_inside(time_ago, Time.now).count} schedulable downloads"
+    Episode.downloadable.airs_at_inside(time_ago, Time.now).all.each do |episode|
       EpisodeDownloader.perform_async episode.id
     end
   end
