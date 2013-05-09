d9275c0a705fd88630ebca4d3ae74200c1743cac
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-05-09 16:46:55 +0200

    Moar cleanup

--- app/controllers/user_area_controller.rb
+++ app/controllers/user_area_controller.rb
@@ -1,10 +1,5 @@
 class UserAreaController < ApplicationController
-  before_filter :get_user
-  
-  def get_user
-    id = params[:user_id] || params[:id]
-    @user = User.find_by_login(id)
-    
-    raise ActiveRecord::RecordNotFound unless @user
+  def user
+    User.find_by_login!(params[:user_id])
   end
 end
