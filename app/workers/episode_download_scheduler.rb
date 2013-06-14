150818bde10571bc8e7340659fbabdb4d4bf6ba1
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-06-15 01:29:53 +0200

    No more squeel

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -2,7 +2,6 @@ class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
   def perform(time_ago=1.hour.ago)
-    Rails.logger.fatal "Found #{Episode.downloadable.airs_at_inside(time_ago, Time.now).count} schedulable downloads"
     Episode.downloadable.airs_at_inside(time_ago, Time.now).all.each do |episode|
       EpisodeDownloader.perform_async episode.id
     end
