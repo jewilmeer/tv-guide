2dc17d41fc798859baa43a6856e3de99480f9628
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-06-26 22:49:52 +0200

    Make user management work again in the admin.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < AdminAreaController
+  respond_to :html
   before_filter :get_user, :except => [:index, :new, :create]
 
   def index
@@ -9,27 +10,9 @@ class Admin::UsersController < AdminAreaController
     @user = User.new
   end
 
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to([:admin, @user], :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   def update
     respond_to do |format|
-      @user.attributes = params[:user]
-      @user.email      = params[:user][:email] if params[:user][:email]
-      @user.trusted    = params[:user][:trusted]
-      @user.admin      = params[:user][:admin]
-      if @user.save
+      if @user.update_attributes user_params
         flash[:notice] = 'Update successful!'
         format.html { redirect_to(:back, :notice => 'User was successfully updated.') }
         format.xml  { head :ok }
@@ -53,7 +36,10 @@ class Admin::UsersController < AdminAreaController
 
   protected
   def get_user
-    @user = User.find_by_login(params[:id])
-    raise ActiveRecord::RecordNotFound unless @user
+    @user = User.find_by_login!(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :login, :trusted, :admin)
   end
 end
