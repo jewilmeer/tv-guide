c780fa36c5a154c63910bbdcd75d20a8e07bcd11
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-08-07 09:38:13 +0200

    Add vendored gems

---
+++ vendor/cache/tvdb_party-a8daa3ea0bfc/lib/tvdb_party/series.rb
@@ -0,0 +1,82 @@
+module TvdbParty
+  class Series
+    attr_reader :client
+    attr_accessor :id, :name, :overview, :seasons, :first_aired, :genres, :network, :rating, :runtime,
+                  :actors, :banners, :air_time, :airs_dayofweek, :status
+
+    def initialize(client, options={})
+      @client         = client
+      @id             = options["id"]
+      @name           = options["SeriesName"]
+      @overview       = options["Overview"]
+      @network        = options["Network"]
+      @runtime        = options["Runtime"]
+      @status         = options["Status"]
+      @air_time       = options['Airs_Time'] if options['Airs_Time']
+      @airs_dayofweek = options['Airs_DayOfWeek']
+
+      if options["Genre"]
+        @genres = options["Genre"][1..-1].split("|")
+      else
+        @genres = []
+      end
+
+      if options["Rating"] && options["Rating"].size > 0
+        @rating = options["Rating"].to_f
+      else
+        @rating = 0
+      end
+
+      begin
+        @first_aired = Date.parse(options["FirstAired"])
+      rescue
+        @first_aired = nil
+      end
+    end
+
+    def get_episode(season_number, episode_number)
+      client.get_episode(self, season_number, episode_number)
+    end
+
+    def posters(language)
+      banners.select{|b| b.banner_type == 'poster' && b.language == language}
+    end
+
+    def fanart(language)
+      banners.select{|b| b.banner_type == 'fanart' && b.language == language}
+    end
+
+    def series_banners(language)
+      banners.select{|b| b.banner_type == 'series' && b.language == language}
+    end
+
+    def season_posters(season_number, language)
+      banners.select{|b| b.banner_type == 'season' && b.banner_type2 == 'season' && b.season == season_number.to_s && b.language == language}
+    end
+
+    def seasonwide_posters(season_number, language)
+      banners.select{|b| b.banner_type == 'season' && b.banner_type2 == 'seasonwide' && b.season == season_number.to_s && b.language == language}
+    end
+
+    def banners
+      @banners ||= client.get_banners(self)
+    end
+
+    def seasons
+      @seasons ||= client.get_seasons(self)
+    end
+
+    def episodes
+      @episodes ||= client.get_all_episodes(self)
+    end
+
+    def actors
+      @actors ||= client.get_actors(self)
+    end
+
+    def season(season_number)
+      seasons.detect{|s| s.number == season_number}
+    end
+
+  end
+end
