1c907babbec97f553336e85f6174b72c657e9dfb
Jan-Willem van der Meer <mail@jewilmeer.nl>
2013-08-21 09:15:09 +0200

    Nicer breadcrumbs

--- spec/controllers/episodes_controller_spec.rb
+++ spec/controllers/episodes_controller_spec.rb
@@ -3,19 +3,18 @@ require 'spec_helper'
 describe EpisodesController do
   describe "show" do
     let(:episode) { create :episode }
-    let(:request) { get :show, id: episode.id }
+    let(:get_request) { get :show, id: episode.id }
 
-    before { request }
-    it { should assign_to :episode }
+    before { get_request }
     it { should respond_with :success }
   end
 
   describe "update" do
     let(:episode) { create :episode }
-    let(:request) { put :update, id: episode.id, format: :js }
+    let(:update_request) { put :update, id: episode.id, format: :js }
 
     context "not logged in" do
-      before { request }
+      before { update_request }
       it { should respond_with :unauthorized }
     end
 
@@ -23,7 +22,8 @@ describe EpisodesController do
       before do
         sign_in create(:user)
         Episode.any_instance.stub(ensure_up_to_date: true)
-        request
+        Episode.any_instance.stub(download: true)
+        update_request
       end
 
       it { should respond_with :success }
