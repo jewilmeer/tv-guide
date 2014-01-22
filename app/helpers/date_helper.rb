5186d84df5c337291e0154840dd6426a77953656
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-01-22 23:00:27 +0100

    Add time_tag to nice_time entries

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,23 +1,25 @@
 module DateHelper
   def pretty_date timestamp, options={}
-    time = Time.parse(timestamp.to_s)
-    case date_diff(time)
-    when 0
-      'Today'
-    when -1
-      'Yesterday'
-    when 1
-      'Tomorrow'
-    when 2..7
-      "Next #{ l(time, :format => :weekday)}"
-    when -7..-2
-      "Last #{ l(time, :format => :weekday)}"
-    else
-      date = time.to_date
-      if date_needs_year? date
-        l date, :format => :with_day
+    time_tag timestamp do
+      time = Time.parse(timestamp.to_s)
+      case date_diff(time)
+      when 0
+        'Today'
+      when -1
+        'Yesterday'
+      when 1
+        'Tomorrow'
+      when 2..7
+        "Next #{ l(time, :format => :weekday)}"
+      when -7..-2
+        "Last #{ l(time, :format => :weekday)}"
       else
-        l date, :format => :short_with_weekday
+        date = time.to_date
+        if date_needs_year? date
+          l date, :format => :with_day
+        else
+          l date, :format => :short_with_weekday
+        end
       end
     end
   end
