4960a1e5543f7af3c6f2f408b04991973f425869
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-01-24 22:10:20 +0100

    Add program active state update
    In addition based upon Genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,22 @@
 class Genre < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
   has_and_belongs_to_many :programs, -> { uniq }
+
+  scope :active, -> { where active: true }
+  scope :inactive, -> { where active: false }
+
+  after_save { Genre.delay.update_program_state(self.id) }
+
+  def update_program_state
+    if active
+      programs.map(&:update_active_state!)
+    else
+      programs.update_all(active: false)
+    end
+  end
+
+  def self.update_program_state(id)
+    genre = Genre.find(id)
+    genre.update_program_state
+  end
 end
