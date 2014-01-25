c5fe75e55b26abd5db9c04c91c7abe5ba69fd0ea
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-01-25 18:57:16 +0100

    Add active flag for networks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125161941) do
+ActiveRecord::Schema.define(version: 20140125164804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,11 +88,12 @@ ActiveRecord::Schema.define(version: 20140125161941) do
   end
 
   create_table "networks", force: true do |t|
-    t.string   "name",                       null: false
+    t.string   "name",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
-    t.integer  "programs_count", default: 0, null: false
+    t.integer  "programs_count", default: 0,    null: false
+    t.boolean  "active",         default: true, null: false
   end
 
   create_table "programs", force: true do |t|
