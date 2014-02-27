9f85651463703f828ba4ca92d9f5e76e67f536f1
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-02-28 00:03:44 +0100

    Decrease update interval for inactive programs

--- spec/models/concerns/tvdb_spec.rb
+++ spec/models/concerns/tvdb_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 describe Concerns::TVDB do
+  let(:klass) do
+    class Dummy
+      extend ActiveModel::Callbacks
+      define_model_callbacks :create
+      include ActiveModel::Validations
+      include Concerns::TVDB
+    end
+  end
+  let(:instance) { klass.new }
 
   describe 'tvdb_updated_tvdb_ids' do
     let(:update_result) do
@@ -44,5 +53,19 @@ describe Concerns::TVDB do
     subject { Program.new }
     it { should allow_value('House of pain').for(:name) }
     it { should_not allow_value('***DUPLICATE').for(:name) }
+  end
+
+  describe "last_checked_within?" do
+    let(:last_checked_at) { 1.month.ago }
+
+    before { instance.stub last_checked_at: last_checked_at }
+
+    it "is false if " do
+      expect(instance.last_checked_within?(2.months)).to be_false
+    end
+
+    it "is true if in future of last_checked_at" do
+      expect(instance.last_checked_within?(2.weeks)).to be_true
+    end
   end
 end
