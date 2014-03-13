595b486d5ec7b4b1d727a4b38d54f25877f9d83d
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-03-13 09:42:56 +0100

    Make sitemap aware of active/inactive programs
    change priority and changefreq according to the active state of the program

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
   end
 
   def sitemap
-    @programs = Program.where(status: 'Continuing').order('updated_at desc')
+    @programs = Program.order('updated_at desc')
     @episodes = Episode.includes(:program).airs_at_inside(1.week.ago, 1.weeks.from_now).order('episodes.updated_at').limit(1000)
   end
 end
