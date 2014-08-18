a642dd45a8426757aa24e0b779c390b46e8b566f
Jan-Willem van der Meer <mail@jewilmeer.nl>
2014-08-19 00:07:43 +0200

    Prefer ENV vars over secretst

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => Rails.application.secrets.aws['access_key_id'],     # required
-    :aws_secret_access_key  => Rails.application.secrets.aws['secret_access_key']
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'] || Rails.application.secrets.aws['access_key_id'], # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'] || Rails.application.secrets.aws['secret_access_key']
   }
   config.fog_directory  = Rails.env.production? ? 'tv-guide' : 'tv-guide-dev' # required
   config.fog_public     = true                                   # optional, defaults to true
