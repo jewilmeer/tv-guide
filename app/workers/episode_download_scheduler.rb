90d7e368d360713a44d1c1f5bd59c2e393d5b537
Jan-Willem van der Meer <mail@jewilmeer.nl>
2015-04-24 11:02:52 +0200

    Simplified workers

--- app/workers/episode_download_scheduler.rb
+++ app/workers/episode_download_scheduler.rb
@@ -2,8 +2,8 @@ class EpisodeDownloadScheduler
   include Sidekiq::Worker
 
   def perform(time_ago=1.hour.ago)
-    Episode.downloadable.airs_at_inside(time_ago, Time.now).all.each do |episode|
-      EpisodeDownloader.perform_async episode.id
+    Episode.downloadable.airs_at_inside(time_ago, Time.now).pluck(:id).each do |episode_id|
+      EpisodeDownloader.perform_async episode_id
     end
   end
 end
--- app/workers/tvdb_program_updater.rb
+++ app/workers/tvdb_program_updater.rb
@@ -2,6 +2,6 @@ class TvdbProgramUpdater
   include Sidekiq::Worker
 
   def perform(tvdb_id)
-    Program.find_by(tvdb_id: tvdb_id).tvdb_full_update
+    Program.find_by(tvdb_id: tvdb_id).try(:tvdb_full_update)
   end
 end
