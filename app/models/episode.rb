b39d21cc8c20ddbf51d5067a3d9712c3236b831e
Jan-Willem van der Meer <mail@jewilmeer.nl>
2015-04-24 11:55:56 +0200

    Don't do thumbs if the program isn't there

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -179,7 +179,7 @@ class Episode < ActiveRecord::Base
     self.title        = tvdb_result.name || 'TBA'
     self.description  = tvdb_result.overview
     self.airdate      = tvdb_result.air_date
-    self.remote_thumb_url = tvdb_result.thumb if program.active?
+    self.remote_thumb_url = tvdb_result.thumb if program.try(:active?)
     self
   end
 
